378d7067dcca9d9a2f3d357817d510a78b0815bb
fiveinfinity <jlachance1@gmail.com>
2016-09-29 13:28:10 -0500

    added two map functions

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ const robots = [
   { name: 'Ratchet', alliance: null }
 ];
 
+const sortedRobots = robots.map(function(bot) {
+  if(knownDecepticons.includes(bot.name)) {
+    return Object.assign({}, bot, {
+      alliance: 'decepticon'
+    });
+  } else {
+    return Object.assign({}, bot, {
+      alliance: 'autobot'
+    });
+  }
+});
+
 const zebraStripes = [
   { width: 9.12, color: null },
   { width: 5.71, color: null },
@@ -21,3 +33,15 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null },
 ];
+
+const coloredZebraStripes = zebraStripes.map(function(zebra, index) {
+  if(index % 2 == 0) {
+    return Object.assign({}, zebra, {
+      color: 'black'
+    });
+  } else {
+    return Object.assign({}, zebra, {
+      color: 'white'
+    });
+  }
+});
