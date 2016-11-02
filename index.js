3b3598900a9eff29f4497a05a47698c5397cc62a
hudbuz <hzb2@cornell.edu>
2016-11-02 13:55:19 -0400

    Done.

--- index.js
+++ index.js
@@ -21,3 +21,37 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null },
 ];
+
+var sortedRobots = robots.map(function(robot) {
+
+    if (knownDecepticons.includes(robot['name'])){
+      return Object.assign({}, robot, {
+      alliance: 'decepticon'
+    })}
+    else {
+        return Object.assign({}, robot, {
+      alliance: 'autobot'
+    })}
+  })
+var coloredZebraStripes = zebraStripes.map(function(zebra, index){
+
+  if (index%2 > 0){
+    return Object.assign({}, zebra,{
+      color: 'white'
+    })
+  }
+  else{
+    return Object.assign({}, zebra,{
+      color: 'black'
+    })
+  }
+})
+
+
+// const transformedAutobotsWithMap = autobots.map(function (autobot) {
+//   return Object.assign({}, autobot, {
+//     strength: autobot.strength * 2,
+//     isTransformed: true,
+//   });
+// });
+//
