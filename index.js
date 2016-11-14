ba155308a43dc60f99cb22e2565ac6bf14273e45
jacob <mailme711@gmail.com>
2016-11-14 12:05:22 -0500

    Done.

--- index.js
+++ index.js
@@ -21,3 +21,27 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null },
 ];
+
+const coloredZebraStripes = zebraStripes.map(function(stripe, i){
+  if (i%2 === 0){
+    return Object.assign({}, stripe, {
+      color: 'black',
+    });
+  } else {
+    return Object.assign({}, stripe, {
+      color: 'white',
+    });
+  }
+});
+
+const sortedRobots = robots.map(function(robot){
+  if (knownDecepticons.includes(robot.name)){
+    return Object.assign({}, robot, {
+      alliance: 'decepticon',
+    });
+  } else {
+    return Object.assign({}, robot, {
+      alliance: 'autobot',
+    });
+  }
+});
