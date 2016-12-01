909c158086a8597319b0006df8014ab47839423a
Jake Brady <jake@cmdrconsulting.com>
2016-12-01 11:59:49 -0700

    done

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ const robots = [
   { name: 'Ratchet', alliance: null }
 ];
 
+const sortedRobots = robots.map(function(robot){
+  var team = 'autobot';
+  if (knownDecepticons.includes(robot.name)){
+    team = 'decepticon';
+  }
+  return Object.assign({}, robot, {
+    alliance: team
+  });
+});
+
 const zebraStripes = [
   { width: 9.12, color: null },
   { width: 5.71, color: null },
@@ -21,3 +31,15 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null },
 ];
+
+const coloredZebraStripes = zebraStripes.map(function(zebra, index){
+  if (index % 2 === 0){
+    return Object.assign({}, zebra, {
+      color: 'black'
+    });
+  } else {
+    return Object.assign({}, zebra, {
+      color: 'white'
+    });
+  }
+});
