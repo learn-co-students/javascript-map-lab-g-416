b7dbbe917dc2aa73257b478d4a18bdd0819c6b71
evanscloud <dijkstrascloud@gmail.com>
2016-12-15 22:11:22 -0500

    Use map function to return new arrays

--- index.js
+++ index.js
@@ -11,6 +11,19 @@ const robots = [
   { name: 'Ratchet', alliance: null }
 ];
 
+var sortedRobots = robots.map( robot => {
+  if (knownDecepticons.includes(robot.name)) {
+    return Object.assign({}, robot, {
+      alliance: "decepticon"
+    })
+  }
+  else {
+    return Object.assign({}, robot, {
+      alliance: "autobot"
+    })
+  }
+});
+
 const zebraStripes = [
   { width: 9.12, color: null },
   { width: 5.71, color: null },
@@ -21,3 +34,16 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null },
 ];
+
+var coloredZebraStripes = zebraStripes.map( (zebra, index) => {
+  if (index % 2 == 0) {
+    return Object.assign({}, zebra, {
+      color: "black"
+    })
+  }
+  else {
+    return Object.assign({}, zebra, {
+      color: "white"
+    })
+  }
+});
