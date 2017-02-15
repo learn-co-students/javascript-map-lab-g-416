e8b06929c83789599756723184faf3d012b51621
Caleb Faught <faughtcj@gmail.com>
2017-02-15 12:32:29 -0600

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ const robots = [
   { name: 'Ratchet', alliance: null }
 ];
 
+const sortedRobots = robots.map((robot) => {
+  if (knownDecepticons.includes(robot.name)) {
+    return Object.assign({}, robot, {
+      alliance: "decepticon"
+    })
+  } else {
+    return Object.assign({}, robot, {
+      alliance: "autobot"
+    })
+  }
+});
+
+////////////////
+// zebraStripes
+///////////////
 const zebraStripes = [
   { width: 9.12, color: null },
   { width: 5.71, color: null },
@@ -21,3 +36,15 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null },
 ];
+
+const coloredZebraStripes = zebraStripes.map((stripe, index) => {
+  if (index % 2 === 0) {
+    return Object.assign({}, stripe, {
+      color: 'black'
+    })
+  } else {
+    return Object.assign({}, stripe, {
+      color: 'white'
+    })
+  }
+});
