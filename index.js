d02b5adcf2f7e497128ea621310daa2645885fa1
morgancfarrow90 <63519344+morgancfarrow90@users.noreply.github.com>
2020-09-21 15:23:06 -0400

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,27 @@ const robots = [
   { name: 'Ratchet', alliance: null }
 ];
 
+let sortedRobots= [];
+
+
+
+robots.map(robot => {
+  if (knownDecepticons.includes(robot.name))
+  {
+    let newBot= Object.assign({}, robot, {alliance: "decepticon"});
+    sortedRobots.push(newBot)
+  }
+  else {
+    let newBot= Object.assign({}, robot, {alliance: "autobot"});
+    sortedRobots.push(newBot)
+  }
+})
+
+console.log(sortedRobots)
+
+
+
+
 const zebraStripes = [
   { width: 9.12, color: null },
   { width: 5.71, color: null },
@@ -21,3 +42,22 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null }
 ];
+
+let coloredZebraStripes= [];
+
+console.log(zebraStripes.length)
+
+zebraStripes.map(stripe=> {
+  let index= zebraStripes.indexOf(stripe);
+    if(index % 2 === 0){
+      let definedStripe= Object.assign({}, stripe, {color: "black"})
+      coloredZebraStripes.push(definedStripe);
+    }
+    else {
+      let definedStripe= Object.assign({}, stripe, {color: "white"})
+      coloredZebraStripes.push(definedStripe);
+    }
+  }
+)
+
+console.log(coloredZebraStripes)
