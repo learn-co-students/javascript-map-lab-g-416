f9b3b0169864a3267068b5d47930dbf64224019e
dlyons8 <dlyons8@binghamton.edu>
2020-11-12 12:45:27 -0500

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ const robots = [
   { name: 'Ratchet', alliance: null }
 ];
 
+const sortedRobots = robots.map(
+  function (robot) {
+    if (knownDecepticons.includes(robot.name)) {
+      return Object.assign({},robot,{alliance: "decepticon"})
+    } else {
+      return Object.assign({},robot,{alliance: "autobot"})
+    }
+  }
+)
+
 const zebraStripes = [
   { width: 9.12, color: null },
   { width: 5.71, color: null },
@@ -21,3 +31,13 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null }
 ];
+
+const coloredZebraStripes = zebraStripes.map(
+  function (stripe, idx) {
+    if (idx % 2 == 0) {
+      return Object.assign({},stripe,{color: "black"})
+    } else {
+      return Object.assign({},stripe,{color: "white"})
+    }
+  }
+)
