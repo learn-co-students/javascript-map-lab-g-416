a83cc7e2f62659dd5497b1cec8bdad01164cd45e
munroe1786 <era3munroe@gmail.com>
2021-03-14 15:05:02 -0500

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const robots = [
   { name: 'Ratchet', alliance: null }
 ];
 
+const sortedRobots = robots.map(robot => {
+  const isDecepticon = knownDecepticons.includes(robot.name);
+  return Object.assign({}, robot, {
+    alliance: isDecepticon ? 'decepticon' : 'autobot'
+  })
+})
+
 const zebraStripes = [
   { width: 9.12, color: null },
   { width: 5.71, color: null },
@@ -21,3 +28,12 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null }
 ];
+
+const coloredZebraStripes = zebraStripes.map((zebraStripe, index) => {
+  const isEven = (index % 2) === 0;
+  return Object.assign({}, zebraStripe, {
+    color: isEven ? 'black' : 'white'
+  })
+})
+
+
